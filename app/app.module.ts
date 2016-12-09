058ea26001e4882feeb3b280ea9a14087756db30
Dennys Lopez Dinza <dldinza.web@gmail.com>
2016-12-09 12:36:22 -0300

    Introduction to Angular Testing   - Tools and technologies   - Setup   - Isolated tests vs. Angular testing utilities
    The first test
      - run with karma
      - debugging karma tests
    
    A simple component test
      - TestBed
      - configureTestingModule
      - createComponent
      - ComponentFixture, DebugElement, query(By.css)
      - detectChanges
      - ComponentFixtureAutoDetect
    
    Test a component with an external template
      - async setup in beforeEach
      - compileComponents

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { BannerComponent } from './banner.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent, BannerComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ app/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-banner',
+  templateUrl: 'banner.component.html',
+  styleUrls: [ 'banner.component.css' ]
+})
+
+export class BannerComponent {
+  title = 'Test Tour of Heroes';
+}
